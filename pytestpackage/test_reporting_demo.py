08a89b2abbb313315a37b922fa72a9115e8d14da
Admin <chetanwankhade281@gmail.com>
2020-02-21 20:11:09 +0530

    Done with html report generation for test case

---
+++ pytestpackage/test_reporting_demo.py
@@ -0,0 +1,24 @@
+#  pip3 install pytest-html
+import pytest
+
+from pytestpackage.class_to_test import SomeClassToTest
+
+
+@pytest.mark.usefixtures("oneTimeSetUp", "setUp")
+class TestClassDemo():
+
+    @pytest.fixture(autouse=True)  # autouse=True is use to available a fixtures throughout(scope) of class
+    def classSetup(self, oneTimeSetUp):
+        self.c = SomeClassToTest(self.value)
+
+    def test_methodA(self, classSetup):
+        result = self.c.sumNumbers(2,3)
+        assert result == 15
+        print(result)
+        print("Running method A")
+
+    def test_methodB(self):
+        result = self.c.sumNumbers(2,3)
+        assert result > 20
+        print(result)
+        print("Running method B")
